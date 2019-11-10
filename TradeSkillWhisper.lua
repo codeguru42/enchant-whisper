62bf2413ccd079d9016b09f66fce5b1dafb84d82
Code-Apprentice <codeguru42@gmail.com>
2019-11-10 11:06:19 -0700

    Only respond to "!list" command

--- TradeSkillWhisper.lua
+++ TradeSkillWhisper.lua
@@ -1,14 +1,16 @@
 local function tradeSkillWhisperEventHandler(self, event, msg, sender)
     if event == "CHAT_MSG_WHISPER" then
-        local count = GetNumCrafts()
-        if count > 0 then
-            for i=1, count do
-                local link = GetCraftItemLink(i);
-                DEFAULT_CHAT_FRAME:AddMessage(link);
+        if msg == "!list" then
+            local count = GetNumCrafts()
+            if count > 0 then
+                for i=1, count do
+                    local link = GetCraftItemLink(i);
+                    SendChatMessage(link, "WHISPER", nil, sender)
+                end
+            else
+                print("You must open enchanting window")
+                return 0
             end
-        else
-            print("You must open enchanting window")
-            return 0
         end
     end
 end
