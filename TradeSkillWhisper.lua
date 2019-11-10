6d403864c433c7d54f3b56d8c50cda576d6d917c
Code-Apprentice <codeguru42@gmail.com>
2019-11-10 11:55:45 -0700

    Refactor a function

--- TradeSkillWhisper.lua
+++ TradeSkillWhisper.lua
@@ -1,16 +1,20 @@
+local function showRecipeLinks(recipient)
+    local count = GetNumCrafts()
+    if count > 0 then
+        for i=1, count do
+            local link = GetCraftItemLink(i);
+            SendChatMessage(link, "WHISPER", nil, recipient)
+        end
+    else
+        print("You must open enchanting window")
+        return 0
+    end
+end
+
 local function tradeSkillWhisperEventHandler(self, event, msg, sender)
     if event == "CHAT_MSG_WHISPER" then
         if msg == "!list" then
-            local count = GetNumCrafts()
-            if count > 0 then
-                for i=1, count do
-                    local link = GetCraftItemLink(i);
-                    SendChatMessage(link, "WHISPER", nil, sender)
-                end
-            else
-                print("You must open enchanting window")
-                return 0
-            end
+            showRecipeLinks(sender)
         end
     end
 end
