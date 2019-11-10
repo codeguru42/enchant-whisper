3ebefbe53133eab2df4137ef1e35dc832afaead2
Code-Apprentice <codeguru42@gmail.com>
2019-11-10 14:14:38 -0700

    Whisper message when no matches found for search term

--- TradeSkillWhisper.lua
+++ TradeSkillWhisper.lua
@@ -2,6 +2,7 @@ local function showRecipeLinks(recipient, ...)
     local term = ...
     local count = GetNumCrafts()
     if count > 0 then
+        local found = 0
         for i=1, count do
             local name, _, _, _, _, _, _ = GetCraftInfo(i)
             local link = GetCraftItemLink(i);
@@ -10,10 +11,16 @@ local function showRecipeLinks(recipient, ...)
                 term = string.lower(term)
                 if string.find(name, term) then
                     SendChatMessage(link, "WHISPER", nil, recipient)
+                    found = found + 1
                 end
             else
                 SendChatMessage(link, "WHISPER", nil, recipient)
+                found = found + 1
             end
+        end
+        if found == 0 then
+            error = string.join(' ', "No matches found for", term)
+            SendChatMessage(error, "WHISPER", nil, recipient)
         end
     else
         print("You must open enchanting window")
