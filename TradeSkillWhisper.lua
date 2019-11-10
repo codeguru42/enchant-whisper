97531436d0efd1cd872cb485a16c6614674d9ddb
Code-Apprentice <codeguru42@gmail.com>
2019-11-10 10:19:43 -0700

    List all enchanting recipes

--- TradeSkillWhisper.lua
+++ TradeSkillWhisper.lua
@@ -1,5 +1,19 @@
 local function tradeSkillWhisperEventHandler(self, event, msg, sender)
-    print(event, sender, msg)
+    if event == "CHAT_MSG_WHISPER" then
+        local count = GetNumCrafts()
+        if count > 0 then
+            for i=1, count do
+                local name, _, type, _, _ = GetCraftInfo(i);
+                print(name, type)
+                if (name and type ~= "header") then
+                    DEFAULT_CHAT_FRAME:AddMessage("Found: "..name);
+                end
+            end
+        else
+            print("You must open enchanting window")
+            return 0
+        end
+    end
 end
 
 local frame = CreateFrame("Frame")
